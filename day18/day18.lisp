c8320d68b71e2222145d150747331390a7731f48
bo-tato <122528427+bo-tato@users.noreply.github.com>
2024-01-13 13:12:50 +0000

    part2

--- day18/day18.lisp
+++ day18/day18.lisp
@@ -1,14 +1,27 @@
 (in-package :advent-of-code-2023)
 
+(defun part1-parse (line)
+  (destructuring-bind (direction distance color) (words line)
+    (declare (ignore color))
+    (list (match direction
+            ("U" :up)
+            ("D" :down)
+            ("R" :right)
+            ("L" :left))
+          (parse-integer distance))))
+
+(defun part2-parse (line)
+  (register-groups-bind (distance direction) ("#(\\p{Hex_Digit}{5})(\\d)" line)
+    (list (match direction
+            ("0" :right)
+            ("1" :down)
+            ("2" :left)
+            ("3" :up))
+          (parse-integer distance :radix 16))))
+
 (loop for position = '(0 . 0) then (move position direction distance)
       for line in (read-file-lines "input.txt")
-      for (dir-char dist-str) = (words line)
-      for direction = (match dir-char
-                        ("U" :up)
-                        ("D" :down)
-                        ("R" :right)
-                        ("L" :left))
-      for distance = (parse-integer dist-str)
+      for (direction distance) = (part2-parse line)
       collect position into points
       sum distance into perimiter
       finally (return (+ (abs (shoelace points)) (/ perimiter 2) 1)))
